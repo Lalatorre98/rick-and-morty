a08f82c2e4eaaaeb5fe3c58b5691398cd4f0d5b2
Lizet Alatorre <lizetalatorre98@gmail.com>
2023-08-19 13:29:58 -0400

    completed Homepage, Header, Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div class="footer-container">
+    <button class="contact-btn">Contact Us</button>
+    </div>
+  )
+}
+
+export default Footer
